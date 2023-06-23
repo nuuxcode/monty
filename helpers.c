74263c3e5b8de17a8aa3b9566a9fb5b4e6a7f897
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-23 17:27:04 +0000

    Task0 finaly, last touch betty

--- helpers.c
+++ helpers.c
@@ -50,7 +50,8 @@ void verify_number(char *token)
 
 	if (atoi(token) < 0)
 		i++;
-	if ((isdigit(token[i]) && strlen(token) == num_len(atoi(token))) || (token[0] == '-' && isdigit(token[1])))
+	if ((isdigit(token[i]) && strlen(token) == num_len(atoi(token))) ||
+		(token[0] == '-' && isdigit(token[1])))
 		datax.push_value = atoi(token);
 	else
 	{
